2caa7c3e9343506b8920d8e75c6c831c39a779c1
Kerem Ozen <keremozzenn@gmail.com>
2025-05-05 12:10:06 +0300

    my city page

---
+++ views/my_city.php
@@ -0,0 +1,80 @@
+<div class="container my-5">
+    <h1 class="text-center mb-4">İstanbul</h1>
+
+    
+    <div id="citySlider" class="carousel slide mb-5" data-bs-ride="carousel" style="max-width: 600px;">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <a href="#ayasofya">
+                <img src="static/ayasofya.jpg" class="d-block w-100 rounded" alt="Ayasofya">
+            </a>
+        </div>
+        <div class="carousel-item">
+            <a href="#topkapi">
+                <img src="static/topkapi.jpg" class="d-block w-100 rounded" alt="Topkapı Sarayı">
+            </a>
+        </div>
+        <div class="carousel-item">
+            <a href="#büyükada">
+                <img src="static/buyukada.jpg" class="d-block w-100 rounded" alt="Büyükada">
+            </a>
+        </div>
+        <div class="carousel-item">
+            <a href="#camlica">
+                <img src="static/camlica.jpg" class="d-block w-100 rounded" alt="Çamlıca Tepesi">
+            </a>
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#citySlider" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#citySlider" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+
+    <div class="row mb-5">
+        <div class="col-md-6">
+        <h3>Nüfus</h3>
+        <p>İstanbul’un 2024 yılı itibariyle nüfusu yaklaşık 15.5 milyondur.</p>
+        <h3>Tarihi Yerler</h3>
+        <ul>
+            <li>Ayasofya Camii</li>
+            <li>Topkapı Sarayı</li>
+            <li>Sultanahmet Camii</li>
+            <li>Kapalıçarşı</li>
+        </ul>
+
+        </div>
+        <div class="col-md-6">
+        <h3>Doğal Güzellikler</h3>
+        <ul>
+            <li>Belgrad Ormanı</li>
+            <li>Emirgan Korusu</li>
+            <li>Büyükada</li>
+            <li>Çamlıca Tepesi</li>
+        </ul>
+        </div>
+    </div>
+
+  <!-- İçerik bölümleri -->
+  <div id="ayasofya" class="mb-4">
+    <h4>Ayasofya</h4>
+    <p>Bizans ve Osmanlı dönemlerine tanıklık eden, mimarisiyle büyüleyen bir yapıdır.</p>
+  </div>
+
+  <div id="topkapi" class="mb-4">
+    <h4>Topkapı Sarayı</h4>
+    <p>Osmanlı padişahlarının yüzyıllar boyunca yaşadığı tarihi saray.</p>
+  </div>
+
+  <div id="büyükada" class="mb-4">
+    <h4>Büyükada</h4>
+    <p>İstanbul’un en büyük adası, doğa ve nostalji dolu bir atmosfer sunar.</p>
+  </div>
+
+  <div id="camlica" class="mb-4">
+    <h4>Çamlıca Tepesi</h4>
+    <p>İstanbul’u kuş bakışı izleyebileceğiniz eşsiz bir manzara noktası.</p>
+  </div>
+</div>
